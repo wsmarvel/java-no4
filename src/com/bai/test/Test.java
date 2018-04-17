b4d83b8fa955b9f6af13511cea31bbaaafc4bdfa
bye <bye@bye-PC>
2018-04-17 14:17:35 +0800

    java-no4

---
+++ src/com/bai/test/Test.java
@@ -0,0 +1,15 @@
+package com.bai.test;
+
+public class Test {
+	public void pupAge() {
+		int age = 0;
+		age = age + 7;
+		System.out.println("小狗的年龄是 "+ age);
+	}
+
+	public static void main(String args[]) {
+		Test test = new Test();
+		test.pupAge();
+		
+	}
+}
